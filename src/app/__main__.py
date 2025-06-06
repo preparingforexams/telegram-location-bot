4a0f8e1b2a7f26a5204fc610dc5cdc75762e4cc3
Björn Petersen <git@bjoernpetersen.net>
2025-06-06 10:47:47 +0200

    Use uvloop

--- src/app/__main__.py
+++ src/app/__main__.py
@@ -1,4 +1,7 @@
+import asyncio
 import logging
+
+import uvloop
 
 from app.bot import Bot
 from app.init import initialize
@@ -7,6 +10,9 @@ _LOG = logging.getLogger(__package__)
 
 if __name__ == "__main__":
     config = initialize()
+
+    asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+
     bot = Bot(config)
     _LOG.info("Running bot")
     bot.run()
